67d1458c9e3940e30702e239f261345bdad88944
rlakdus <166114526+rlakdus@users.noreply.github.com>
2026-01-21 21:01:35 +0900

    add base preprocessing

--- review_analysis/preprocessing/main.py
+++ review_analysis/preprocessing/main.py
@@ -3,13 +3,13 @@ import glob
 from argparse import ArgumentParser
 from typing import Dict, Type
 from review_analysis.preprocessing.base_processor import BaseDataProcessor
-from review_analysis.preprocessing.ridibooks_processor import RidibooksProcessor
+from review_analysis.preprocessing.example_processor import ExampleProcessor
 
 
 # 모든 preprocessing 클래스를 예시 형식으로 적어주세요. 
 # key는 "reviews_사이트이름"으로, value는 해당 처리를 위한 클래스
 PREPROCESS_CLASSES: Dict[str, Type[BaseDataProcessor]] = {
-    "reviews_ridibooks": RidibooksProcessor,
+    "reviews_example": ExampleProcessor,
     # key는 크롤링한 csv파일 이름으로 적어주세요! ex. reviews_naver.csv -> reviews_naver
 }
 
