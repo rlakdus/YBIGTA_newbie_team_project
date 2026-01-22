3c60ab070260136818052a731582d5737fd2111f
qkrwnsqja8996 <qkrwnsqja8996@yonsei.ac.kr>
2026-01-22 17:27:08 +0900

    Add files via upload
    processing 수정

--- review_analysis/preprocessing/main.py
+++ review_analysis/preprocessing/main.py
@@ -1,15 +1,25 @@
+import sys
+from pathlib import Path
+
+# 현재 파일(main.py) 기준으로 루트 경로를 잡고 sys.path에 추가합니다.
+FILE_PATH = Path(__file__).resolve()
+ROOT_DIR = FILE_PATH.parent.parent.parent
+sys.path.append(str(ROOT_DIR))
+
+# glob이 사용하는 상대 경로("../../")가 터미널 실행 위치에 상관없이 항상 'ROOT_DIR'를 가리키도록 작업 경로(CWD)를 강제 변경
 import os
+os.chdir(FILE_PATH.parent)
+
 import glob
 from argparse import ArgumentParser
 from typing import Dict, Type
 from review_analysis.preprocessing.base_processor import BaseDataProcessor
-from review_analysis.preprocessing.example_processor import ExampleProcessor
-
+from review_analysis.preprocessing.yes24_processor import Yes24Processor
 
 # 모든 preprocessing 클래스를 예시 형식으로 적어주세요. 
 # key는 "reviews_사이트이름"으로, value는 해당 처리를 위한 클래스
 PREPROCESS_CLASSES: Dict[str, Type[BaseDataProcessor]] = {
-    "reviews_example": ExampleProcessor,
+    "reviews_yes24": Yes24Processor,
     # key는 크롤링한 csv파일 이름으로 적어주세요! ex. reviews_naver.csv -> reviews_naver
 }
 
